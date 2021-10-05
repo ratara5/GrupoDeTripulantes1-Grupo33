aec28f931a23cbd44c3a2d5e999062568af8bb47
ratara5 <ratara5@gmail.com>
2021-10-05 12:37:50 -0500

    Iconos y categorías en Menu.vue

--- Habit_Tracker/client/src/components/Menu.vue
+++ Habit_Tracker/client/src/components/Menu.vue
@@ -9,18 +9,59 @@
             alt="Ecommerce"
           />
         </router-link>
+        <template v-for="category in categories" :key="category.id">
+          <router-link class="item" :to="category.slug">
+            {{category.title}}
+          </router-link>
+        </template>        
       </div>
       <div class="right menu">
-        <router-link class="item" to="/login"> Iniciar Sesion </router-link> |
+        <router-link class="item" to="/login" v-if="!token"> Iniciar Sesion </router-link> |
         <router-link class="item" to="/contacto"> Contáctanos </router-link>
+      
+      <template v-if="token">
+        <router-link class="item" to="/orders">Pedidos</router-link>
+        <span class="ui item cart"><i class="shopping cart icon"></i></span>
+        <span class="ui item logout" @click="logout"><i class="sign-out icon"></i></span>
+      </template>
+      
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {ref, onMounted} from 'vue';
+import { getTokenApi, deleteTokenApi } from '../api/token.js'
+import {getCategoriesApi} from  '../api/category';
+
 export default {
   name: "menu",
+
+  setup(){
+
+    let categories=ref(null);
+
+    onMounted(async() =>{
+      const response=await getCategoriesApi();
+      categories.value=response;
+    });
+
+    const token=getTokenApi();
+    
+    const logout= () =>{
+      console.log("Cerrar sesión");    
+      deleteTokenApi();
+      location.replace('/')
+    };
+
+
+    return{
+      token,
+      logout,
+      categories,
+    }
+  }
 };
 </script>
 
